f3ba3689655f36d84a7664ba03502a622ad02f75
DESKTOP-EK6VPE3\nowot <nowotny.damian@gmail.com>
2020-10-11 13:24:57 +0200

    missing figures components added

--- BreadCrumbsDemo/src/app/figures/rectangle/rectangle.module.ts
+++ BreadCrumbsDemo/src/app/figures/rectangle/rectangle.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RectangleComponent } from './rectangle.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [RectangleComponent],
   imports: [
     CommonModule
   ]
