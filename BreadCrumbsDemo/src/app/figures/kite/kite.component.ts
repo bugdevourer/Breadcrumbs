5ae63983f2eddfc8746128c330da8c685b689122
DESKTOP-EK6VPE3\nowot <nowotny.damian@gmail.com>
2020-10-11 14:13:13 +0200

    kite added

---
+++ BreadCrumbsDemo/src/app/figures/kite/kite.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kite',
+  templateUrl: './kite.component.html',
+  styleUrls: ['./kite.component.scss']
+})
+export class KiteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
